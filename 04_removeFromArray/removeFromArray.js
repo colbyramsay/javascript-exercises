52eeb6ac3e54e070490212394272e2071ac1df18
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-01-14 14:01:17 -0800

    Add conditional if/else to check for non present values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,20 @@
 const removeFromArray = function(myArr, ...theArgs) {
     
+    /*
     for (let i = 0; i < theArgs.length; i++) {
         let position = myArr.indexOf(theArgs[i]);
         myArr.splice(position, 1);
+    }
+    */
+
+    for (let i = 0; i < theArgs.length; i++) {
+        if (myArr.includes(theArgs[i])) {
+            let position = myArr.indexOf(theArgs[i]);
+            myArr.splice(position, 1);
+        }
+        else {
+            continue;
+        }
     }
 
     return myArr;
