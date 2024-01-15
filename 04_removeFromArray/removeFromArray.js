df142a6b7e58ba7304ad994a2f7cfe34e2fe68c7
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-01-14 21:50:52 -0800

    Remove unnecessary code & enable all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,5 @@
 const removeFromArray = function(myArr, ...theArgs) {
     
-    /*
-    for (let i = 0; i < theArgs.length; i++) {
-        let position = myArr.indexOf(theArgs[i]);
-        myArr.splice(position, 1);
-    }
-    */
-
     for (let i = 0; i < theArgs.length; i++) {
         if (myArr.includes(theArgs[i])) {
             let position = myArr.indexOf(theArgs[i]);
