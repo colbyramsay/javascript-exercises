b20f8dbcd05c0d34f1e8980d6c0b13266fd8bbb0
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-01-14 22:52:42 -0800

    Add if, else if to test for negative numbers etc

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,11 +2,19 @@ const sumAll = function(num1, num2) {
 
     let sum = 0;
 
-    for (let i = num1; i <= num2; i++) {
-        sum += i;
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+        return sum;
+    } else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+        return sum;
     }
-
-    return sum;
 
 };
 
