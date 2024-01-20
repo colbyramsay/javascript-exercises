212c8ff93026de6e2908df19fb8e128eb72b26d4
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-01-20 10:40:05 -0800

    Enable & pass all tests for exercise 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,40 @@
-const palindromes = function () {
+
+
+const palindromes = function (text) {
+
+    let string = "";
+    let newTextNoPunc = "";
+    const myArr = text.split("");
+    myArr.reverse();
+
+    const newArr = myArr.filter(removePunc);
+
+    function removePunc(a) {
+        if (a != "!" && a != "," && a != "." && a != " " && a != "?")
+        return true;
+    }
+
+    for (let i = 0; i < newArr.length; i++) {
+        string += newArr[i];
+    }
+
+    const newText = text.split("");
+    const newTextFiltered = newText.filter(removePunc);
+
+    for (let i = 0; i < newTextFiltered.length; i++) {
+        newTextNoPunc += newTextFiltered[i];
+    }
+
+    if (string.toLowerCase() == newTextNoPunc.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
+//palindromes("A car, a man, a maraca.");
+
 // Do not edit below this line
 module.exports = palindromes;
+
