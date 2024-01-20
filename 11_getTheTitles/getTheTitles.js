120ef069fc0eea90fe0fdea65e5e550cb1511083
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-01-20 15:45:12 -0800

    Map titles from object for ex.11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,31 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    
+    //alert("HELLO");
+    
+    const titles = books.map(function(book) {
+        return book.title;
+    });
+
+    return titles;
+
+    //console.log(titles);
 
 };
+
+/*
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+];
+
+getTheTitles(books);
+*/
 
 // Do not edit below this line
 module.exports = getTheTitles;
