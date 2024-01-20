92f772e79a124005694c3109024246fce4c301f7
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-01-20 09:21:31 -0800

    Enable & pass all tests for exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,43 @@
-const add = function(num1, num2) {
+const add = function(x, y) {
   let result = 0;
-  result = num1 + num2;
+  result = x + y;
   return result;
 };
 
-const subtract = function(num1, num2) {
+const subtract = function(x, y) {
   let result = 0;
-  result = num1 - num2;
+  result = x - y;
   return result;
 };
 
 const sum = function(myArr) {
 	let result = 0;
-  for (let i = 0; i <= myArr.length-1; i++) {
+  for (let i = 0; i < myArr.length; i++) {
     result += myArr[i];
   }
   return result;
 };
 
-const multiply = function() {
-
+const multiply = function(myArr) {
+  let z = 1;
+  for (let i = 0; i < myArr.length; i++) {
+    z = z * myArr[i];
+  }
+  return z;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let result = 0;
+  result = x ** y;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let y = 1;
+	for (let i = 1; i < x; i++) {
+    y = y * (i+1);
+  }
+  return y;
 };
 
 // Do not edit below this line
