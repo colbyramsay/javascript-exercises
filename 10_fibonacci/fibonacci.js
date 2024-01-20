eb898a35874ea363c854896eeab6abe7717dba20
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-01-20 14:51:07 -0800

    Set up initial fibonacci array for ex.10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
 const fibonacci = function() {
-
+    const myArr = [1,1];
+    for (i = 2; i <= 25; i++) {
+        myArr[i] = myArr[i-2] + myArr[i-1];
+    }
+    alert (myArr);
 };
 
+fibonacci();
+
 // Do not edit below this line
+//1, 1, 2, 3, 5, 8,
 module.exports = fibonacci;
