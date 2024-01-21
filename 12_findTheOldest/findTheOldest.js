ea388044a7b32f10cf95a81f8d5dd4f1d6304332
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-01-20 16:17:29 -0800

    Enable & pass find the oldest ex12_findTheOldest test #1

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    const oldest = people.sort(function(a, b) {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        
+        if (lastPerson > nextPerson) {
+            return -1;
+        } else {
+            return 1;
+        }
+
+    });
+
+    //console.log(oldest[0].name);
+    return oldest[0];
 
 };
+
+/*
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ];
+
+  findTheOldest(people);
+  */
 
 // Do not edit below this line
 module.exports = findTheOldest;
